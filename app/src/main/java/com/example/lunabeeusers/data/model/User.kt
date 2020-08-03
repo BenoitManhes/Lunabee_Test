c1dae9e55172f6f30a31273e1e2c7ab1e200b24d
Unknown <benoit.manhes.82@gmail.com>
2020-08-03 19:57:07 +0200

    fix minor bugs

---
+++ app/src/main/java/com/example/lunabeeusers/data/model/User.kt
@@ -0,0 +1,15 @@
+package com.example.lunabeeusers.data.model
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(
+    @Json(name = "id")val id: Double,
+    @Json(name = "first_name")val firstname: String,
+    @Json(name = "last_name")val lastname: String,
+    @Json(name = "email")val email: String,
+    @Json(name = "gender")val gender: String,
+    @Json(name = "avatar") val imgSrcUrl: String
+) : Parcelable
