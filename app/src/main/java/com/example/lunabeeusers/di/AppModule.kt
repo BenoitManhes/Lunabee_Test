ee74dd6f5d054c35fbcdc5d29bb77e6e04864150
Unknown <benoit.manhes.82@gmail.com>
2020-08-05 19:20:37 +0200

    App module

--- app/src/main/java/com/example/lunabeeusers/di/AppModule.kt
+++ app/src/main/java/com/example/lunabeeusers/di/AppModule.kt
@@ -1,4 +1,42 @@
 package com.example.lunabeeusers.di
 
+import com.example.lunabeeusers.data.remote.ApiService
+import com.example.lunabeeusers.data.repository.UserRepository
+import com.example.lunabeeusers.utils.Constant
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi): Retrofit = Retrofit.Builder()
+        .baseUrl(Constant.BASE_URL)
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    @Provides
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideRepository(apiService: ApiService) =
+        UserRepository(apiService)
 }
