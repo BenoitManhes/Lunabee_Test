9b804371f1d89a303c6a2bceede876f67818324e
Unknown <benoit.manhes.82@gmail.com>
2020-08-05 13:23:00 +0200

    fix dependencies

--- app/src/main/java/com/example/lunabeeusers/ui/overview/UserListAdapter.kt
+++ app/src/main/java/com/example/lunabeeusers/ui/overview/UserListAdapter.kt
@@ -67,8 +67,6 @@ class UserListAdapter : ListAdapter<User, UserListAdapter.UserViewHolder>(DiffCa
                 if (parent.getChildAdapterPosition(view) == 0) {
                     top = spaceHeight
                 }
-                //                left =  spaceHeight
-                //                right = spaceHeight
                 bottom = spaceHeight
             }
         }
