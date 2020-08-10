78d06798224cb164503acdf6e0960c1b6eee9036
Unknown <benoit.manhes.82@gmail.com>
2020-08-10 19:01:10 +0200

    add search to the list

--- app/src/main/java/com/example/lunabeeusers/ui/MainActivity.kt
+++ app/src/main/java/com/example/lunabeeusers/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lunabeeusers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.lunabeeusers.R
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/example/lunabeeusers/ui/overview/UserOverviewFragment.kt
+++ app/src/main/java/com/example/lunabeeusers/ui/overview/UserOverviewFragment.kt
@@ -2,8 +2,13 @@ package com.example.lunabeeusers.ui.overview
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -24,10 +29,12 @@ class UserOverviewFragment : Fragment() {
     private lateinit var binding: UserOverviewFragmentBinding
     private lateinit var fastAdapter: FastAdapter<User>
     private val itemAdapter = ItemAdapter<User>()
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         binding = UserOverviewFragmentBinding.inflate(inflater)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -39,6 +46,44 @@ class UserOverviewFragment : Fragment() {
         setupObservers()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        val searchItem = menu.findItem(R.id.share)
+        searchItem?.let {
+            setupSearchView(searchItem)
+        }
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun setupSearchView(item: MenuItem) {
+        // Setup searchItem expansion
+        item.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                return true
+            }
+
+            // Remove filter when search action is collapsed
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                viewModel.clearUserFilter()
+                return true
+            }
+        })
+
+        // Setup query user filter
+        searchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.searchUser(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.searchUser(newText)
+                return true
+            }
+        })
+    }
+
     private fun setupRecyclerView() {
         fastAdapter = FastAdapter.with(itemAdapter)
 
@@ -46,13 +91,13 @@ class UserOverviewFragment : Fragment() {
         // Add Decorator to RecyclerView
         binding.usersRv.addItemDecoration(
             MarginItemDecoration(
-                resources.getDimension(R.dimen.short_margin).toInt()
+                resources.getDimension(R.dimen.item_spacing).toInt()
             )
         )
     }
 
     private fun setupSwipRefreshLayout() {
-        binding.swiperefresh.setColorSchemeResources(R.color.purple_light)
+        binding.swiperefresh.setColorSchemeResources(R.color.colorBackgroundDark)
         binding.swiperefresh.setOnRefreshListener {
             viewModel.refreshData()
         }
@@ -78,6 +123,16 @@ class UserOverviewFragment : Fragment() {
                 updateStatutUi(it)
             }
         })
+
+        // Observing search term
+        viewModel.searchTerm.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                itemAdapter.filter(it)
+                itemAdapter.itemFilter.filterPredicate = { user: User, constraint: CharSequence? ->
+                    user.isConcernedByTerm(constraint.toString())
+                }
+            }
+        })
     }
 
     /**
@@ -93,12 +148,18 @@ class UserOverviewFragment : Fragment() {
         binding.tryAgain.visibility = if (nothingToShow && statut.equals(Statut.ERROR)) View.VISIBLE else View.GONE
 
         if (!nothingToShow && statut.equals(Statut.ERROR)) {
-            Snackbar.make(binding.root, R.string.sync_failed, Snackbar.LENGTH_SHORT).show()
+            showSnackBar(R.string.sync_failed)
         }
 
         if (!statut.equals(Statut.LOADING)) {
             binding.swiperefresh.isRefreshing = false
         }
+    }
+
+    private fun showSnackBar(messageSrc: Int) {
+        val snackbar = Snackbar.make(binding.root, messageSrc, Snackbar.LENGTH_SHORT)
+        snackbar.view.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.colorBackgroundDark))
+        snackbar.show()
     }
 
 }
